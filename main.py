d0815ca64002eae480a309583d65116f4367e4e8
hzlhn96 <546181670@qq.com>
2025-05-30 15:21:58 +0800

    Add files

---
+++ main.py
@@ -0,0 +1,42 @@
+import logging.config
+import traceback
+from logging import getLogger
+
+import discord
+from discord.ext import commands
+from setup import TOKEN
+from discord import app_commands
+
+logging.config.fileConfig('logging.conf')
+logger = getLogger(__name__)
+
+INITIAL_EXTENSIONS = [
+    'cogs.clan_battle',
+    'cogs.error_handler'
+]
+
+class MyBot(commands.Bot):
+    def __init__(self, command_prefix, intents):
+        super().__init__(command_prefix, intents=intents, help_command=None)
+        for cog in INITIAL_EXTENSIONS:
+            try:
+                self.load_extension(cog)
+            except Exception:
+                traceback.print_exc()
+
+    async def setup_hook(self):
+        await self.tree.sync()
+
+    async def on_ready(self):
+        logger.info("Login was successful.")
+        logger.info(f"bot name: {self.user.name}")
+        logger.info(f"bot id: {self.user.id}")
+
+async def main():
+    intents = discord.Intents(messages=True, guilds=True, members=True, reactions=True)
+    bot = MyBot('.', intents)
+    await bot.start(TOKEN)
+
+if __name__ == '__main__':
+    import asyncio
+    asyncio.run(main())
---
+++ setup.sql
@@ -0,0 +1,95 @@
+create table ClanData (
+    guild_id int,
+    category_id int,
+    boss1_channel_id int,
+    boss2_channel_id int,
+    boss3_channel_id int,
+    boss4_channel_id int,
+    boss5_channel_id int,
+    remain_attack_channel_id int,
+    reserve_channel_id int,
+    command_channel_id int,
+    boss1_reserve_message_id int,
+    boss2_reserve_message_id int,
+    boss3_reserve_message_id int,
+    boss4_reserve_message_id int,
+    boss5_reserve_message_id int,
+    remain_attack_message_id int,
+    summary_channel_id int,
+    day date
+);
+create table PlayerData (
+    category_id int,
+    user_id int,
+    physics_attack int default 0,
+    magic_attack int default 0,
+    task_kill boolean
+);
+
+create table ReserveData (
+    category_id int,
+    boss_index int,
+    user_id int,
+    attack_type varchar,
+    damage int,
+    memo varchar,
+    carry_over boolean
+);
+
+create table AttackStatus (
+    category_id int,
+    user_id int,
+    lap int,
+    boss_index int,
+    damage int,
+    memo varchar,
+    attacked boolean,
+    attack_type varchar,
+    carry_over boolean,
+    created datetime
+);
+
+create table BossStatusData (
+    category_id int,
+    boss_index int,
+    lap int,
+    beated boolean
+);
+
+create table CarryOver (
+    category_id int,
+    user_id int,
+    boss_index int,
+    attack_type varchar,
+    carry_over_time int,
+    created datetime
+);
+
+create table FormData (
+    category_id int,
+    form_url varchar,
+    sheet_url varchar,
+    name_entry varchar,
+    discord_id_entry varchar,
+    created datetime
+);
+
+create table ProgressMessageIdData (
+    category_id int,
+    lap int,
+    boss1 int,
+    boss2 int,
+    boss3 int,
+    boss4 int,
+    boss5 int
+);
+
+create table SummaryMessageIdData (
+    category_id int,
+    lap int,
+    boss1 int,
+    boss2 int,
+    boss3 int,
+    boss4 int,
+    boss5 int
+);
